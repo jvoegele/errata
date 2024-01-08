363ce0df03e48acd4e4969e9623acbb25aa6e028
Jason Voegele <jason.voegele@gmail.com>
2024-01-08 16:32:16 -0500

    Initial project skeleton generated by `mix new`

---
+++ lib/errata.ex
@@ -0,0 +1,18 @@
+defmodule Errata do
+  @moduledoc """
+  Documentation for `Errata`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Errata.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/errata_test.exs
@@ -0,0 +1,8 @@
+defmodule ErrataTest do
+  use ExUnit.Case
+  doctest Errata
+
+  test "greets the world" do
+    assert Errata.hello() == :world
+  end
+end
