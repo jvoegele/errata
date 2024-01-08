b9473ba963754fabaacdb872d9dde1f51eccf833
Jason Voegele <jason.voegele@gmail.com>
2024-01-08 16:50:23 -0500

    Add some dev dependencies

--- lib/errata.ex
+++ lib/errata.ex
@@ -2,17 +2,4 @@ defmodule Errata do
   @moduledoc """
   Documentation for `Errata`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Errata.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/errata_test.exs
+++ test/errata_test.exs
@@ -1,8 +1,4 @@
 defmodule ErrataTest do
   use ExUnit.Case
   doctest Errata
-
-  test "greets the world" do
-    assert Errata.hello() == :world
-  end
 end
